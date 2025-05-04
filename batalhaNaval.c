5dde6f91fd09be8d748235dab120bc44a42e33ae
kawansantanan <kawansantana597@gmail.com>
2025-05-04 01:57:26 +0000

    navio 3 e 4 em posição

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,6 +5,8 @@ int main(){
     int tabuleiro[10][10];
     int navio1[3] = { 3, 3, 3};
     int navio2[3] = { 3, 3, 3};
+    int navio3[1] = { 3 };
+    int navio4[1] = { 3 };
 
    for(int i = 0; i < 10; i++){
       for(int j = 0; j < 10; j++){
@@ -18,6 +20,17 @@ int main(){
 
     for (int i = 0; i < 3; i++){
         tabuleiro[i + 7][7] = navio2[i];
+    }
+    for(int i = 0; i < 3; i++){
+        tabuleiro[5][2] = navio3[i];
+        tabuleiro[6][1] = navio3[i];
+        tabuleiro[7][0] = navio3[i];
+    }
+
+    for (int i = 0; i < 3; i++){
+        tabuleiro[0][0] = navio4[i];
+        tabuleiro[1][1] = navio4[i];
+        tabuleiro[2][2] = navio4[i];
     }
 
     for (int i = 0; i < 10; i++){
